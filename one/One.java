1945749b1f9709f9dbf813f1af268ffe8dcfe75b
Yash Karandikar <nerdstep710@gmail.com>
2025-12-02 10:00:33 -0600

    day 1 part 2

--- one/One.java
+++ one/One.java
@@ -12,13 +12,15 @@ public class One {
             String line = scanner.nextLine();
             char direction = line.charAt(0);
             int sign = direction == 'L' ? -1 : 1;
-            int dist = Integer.parseInt(line.substring(1)) % 100;
+            int dist = Integer.parseInt(line.substring(1));
 
-
-            dial += sign * dist;
-            dial += 100;
-            dial %= 100;
-            if (dial == 0) res++;
+            for (int i = 0; i < dist; i++) {
+                dial += sign;
+                dial += 100;
+                dial %= 100;
+                if (dial == 0) res++;
+                // System.out.println(dial);
+            }
         }
 
         System.out.println(res);
