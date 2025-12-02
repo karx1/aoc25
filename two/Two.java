db684f1ed6451cc7756b7c9359f3386ffb94cf19
Yash Karandikar <nerdstep710@gmail.com>
2025-12-02 10:25:48 -0600

    day 2 part 2

--- two/Two.java
+++ two/Two.java
@@ -15,14 +15,31 @@ public class Two {
 
             for (long i = start; i <= end; i++) {
                 String s = Long.toString(i);
-                if (s.length() % 2 != 0) continue;
 
-                int splitIndex = s.length() / 2;
-                String first = s.substring(0, splitIndex);
-                String second = s.substring(splitIndex);
-                if (first.equals(second)) {
-                    res += i;
+                boolean invalid = false;
+                for (int j = 1; j <= s.length(); j++) {
+                    if (s.length() % j != 0) continue;
+
+                    int splitIndex = s.length() / j;
+                    int pos = 0;
+                    String first = s.substring(pos, pos + splitIndex);
+                    boolean holds = true;
+                    if (first.equals(s)) holds = false;
+                    for (int k = 0; k < j - 1; k++) {
+                        pos += splitIndex;
+                        String sub = s.substring(pos, pos + splitIndex);
+                        if (!first.equals(sub)) {
+                            holds = false;
+                        }
+                    }
+
+                    if (holds) {
+                        invalid = true;
+                        break;
+                    }
                 }
+
+                if (invalid) res += i;
             }
         }
 
