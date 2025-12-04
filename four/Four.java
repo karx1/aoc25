c8c93a68c29f0b88121824d7f03e87ed7777cc27
Yash Karandikar <nerdstep710@gmail.com>
2025-12-04 02:22:17 -0600

    day 4 part 2

--- four/Four.java
+++ four/Four.java
@@ -3,29 +3,40 @@ import java.util.Scanner;
 
 public class Four {
     public static void main(String[] args) {
-        ArrayList<String> grid = new ArrayList<>();
+        ArrayList<StringBuilder> grid = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNextLine()) {
-            grid.add(scanner.nextLine());
+            grid.add(new StringBuilder(scanner.nextLine()));
         }
 
         int res = 0;
         int[][] dirs = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
 
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid.get(i).length(); j++) {
-                char c = get(i, j, grid);
-                if (c == '@') {
-                    int num = 0;
+        while (true) {
+            boolean didRemove = false;
+	        for (int i = 0; i < grid.size(); i++) {
+	            for (int j = 0; j < grid.get(i).length(); j++) {
+	                char c = get(i, j, grid);
+	                if (c == '@') {
+	                    int num = 0;
+	
+	                    for (int[] dir : dirs) {
+	                        Character other = get(i + dir[0], j + dir[1], grid);
+	                        if (other != null && ((char) other) == '@') num++;
+	                    }
+	
+	                    if (num < 4) {
+                            res++;
+                            grid.get(i).setCharAt(j, '.');
+                            didRemove = true;
+                        }
+	                }
+	            }
+	        }
 
-                    for (int[] dir : dirs) {
-                        Character other = get(i + dir[0], j + dir[1], grid);
-                        if (other != null && ((char) other) == '@') num++;
-                    }
-
-                    if (num < 4) res++;
-                }
+            if (!didRemove) {
+                break;
             }
         }
 
@@ -33,7 +44,7 @@ public class Four {
         scanner.close();
     }
 
-    public static Character get(int i, int j, ArrayList<String> grid) {
+    public static Character get(int i, int j, ArrayList<StringBuilder> grid) {
         if (i < 0 || j < 0 || i >= grid.size() || j >= grid.get(0).length()) {
             return null;
         }
